c5a9f774992e9bddb282042c50189c1cd473a2e3
Wioletta Koperska <koperskawioletta@gmail.com>
2025-01-27 19:11:12 +0100

    change

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// Funkcja powitalna
+const welcome = () => {
+    console.log("Witaj na mojej stronie!");
+};
+
+// Zmiana tła kontenera
+const onChangeBackgroundClick = () => {
+    const container = document.querySelector(".container");
+    const button = document.querySelector(".changeBackground");
+
+    // Obsługa przełączania klas dla różnych kolorów tła
+    if (container.classList.contains("whiteBackground")) {
+        container.classList.remove("whiteBackground");
+        container.classList.add("darkBackground");
+        button.innerText = "Włącz jasne tło";
+    } else if (container.classList.contains("darkBackground")) {
+        container.classList.remove("darkBackground");
+        container.classList.add("whiteBackground");
+        button.innerText = "Włącz ciemne tło";
+    } else {
+        container.classList.add("whiteBackground");
+        button.innerText = "Włącz ciemne tło";
+    }
+};
+
+// Obsługa przycisku ukrycia obrazka
+const onButtonClick = () => {
+    const headerMain = document.querySelector(".header_main");
+    const button = document.querySelector(".button");
+
+    if (headerMain.style.display === "none") {
+        headerMain.style.display = "block";
+        button.innerText = "Schowaj Obrazek.";
+    } else {
+        headerMain.style.display = "none";
+        button.innerText = "Pokaż Obrazek.";
+    }
+};
+
+// Inicjalizacja funkcji
+const init = () => {
+    const changeColorButton = document.querySelector(".changeBackground");
+    const toggleImageButton = document.querySelector(".button");
+
+    // Obsługa kliknięcia zmiany tła
+    changeColorButton.addEventListener("click", onChangeBackgroundClick);
+
+    // Obsługa kliknięcia ukrycia/pokazania obrazka
+    toggleImageButton.addEventListener("click", onButtonClick);
+
+    welcome();
+};
+
+// Uruchomienie skryptu
+init();
+
+// Sprawdzenie klasy kontenera
+const containerDark = document.querySelector(".containerDark");
+console.log(containerDark);
+
+if (containerDark && containerDark.classList.contains("containerDark")) {
+    console.log("Kontener jest ciemny!");
+} else {
+    console.log("Kontener nie jest ciemny!");
+}
